c68b6c98604b4e329a2866af825175981ed7eb3b
anorekarl <104476707+iwontevenfib@users.noreply.github.com>
2025-06-22 22:34:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,260 @@
+
+
+// data = pokemon
+
+const card = document.querySelector('#poke-container')
+
+const loadButton = document.getElementById('find-button')
+const loading = document.getElementById('loading')
+
+
+let searchOn = false;
+let firstRun = false;
+
+function loader() {
+
+
+    loading.style.display = 'flex'
+    loadButton.style.display = 'none'
+
+}
+
+
+
+setTimeout(fetchData, 2000)
+
+let pokeCounter = 0;
+
+async function fetchData() {
+
+    try {
+
+        card.style.justifyContent = "center"
+
+        if (searchOn) {
+            card.innerHTML = ""
+            searchOn = false;
+        }
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+
+
+
+        if (!response.ok) {
+            throw new Error(`ERROR status: ${response.status}`);
+        }
+
+        //data
+        const data = await response.json();
+
+        // for (let i = pokeCounter; i < 12 + pokeCounter; i++) {
+        //     fetchPokemonData(data.results[i])
+        // }
+
+        for (let i = pokeCounter; i < 12 + pokeCounter; i++) {
+            fetchPokemonData(data.results[randomizer(1025, 1)])
+        }
+
+
+        pokeCounter += 12;
+
+
+    }
+    catch (error) {
+        console.error(error)
+    }
+
+    //Footer
+
+    const footer = document.getElementById('footer');
+
+    footer.innerHTML = `
+   <p>Footer</p>
+   `
+
+    footer.style.display = 'block'
+
+
+    loading.style.display = 'none'
+    loadButton.style.display = 'block'
+
+}
+
+async function searchData() {
+    try {
+
+        searchOn = true;
+
+        card.innerHTML = ""
+        card.style.justifyContent = "space-around"
+
+
+        const searchInput = document.getElementById('poke-finder');
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${capitalizeFirstLetter(searchInput.value)}`)
+
+        const data = await response.json()
+
+        if (searchInput.value == "") {
+
+            setTimeout(fetchData, 1500)
+
+
+        } else {
+            generatePokemon(data)
+            pokeCounter = 0
+        }
+
+
+
+    }
+    catch (error) {
+
+    }
+
+}
+
+
+
+
+
+async function fetchPokemonData(pokemon) {
+    const dataUrl = pokemon.url
+
+
+    try {
+        const response = await fetch(dataUrl)
+
+        const data = await response.json()
+        // console.log(data);
+        generatePokemon(data);
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+async function generatePokemon(pokemon) {
+
+    const pokeInfoBox = document.createElement('div')
+    pokeInfoBox.classList.add('poke-info')
+
+    const pokeSprite = document.createElement('img')
+    pokeSprite.src = pokemon.sprites.other['official-artwork'].front_default;
+
+    const pokeName = document.createElement('h2')
+    pokeName.innerText = capitalizeFirstLetter(pokemon.name)
+
+
+    const pokeId = document.createElement('p')
+    pokeId.classList.add('poke-id')
+    if (pokemon.id < 10) {
+        pokeId.innerHTML = `#000${pokemon.id}`
+    } else if (pokemon.id < 100) {
+        pokeId.innerHTML = `#00${pokemon.id}`
+    } else if (pokemon.id < 1000) {
+        pokeId.innerHTML = `#0${pokemon.id}`
+    } else if (pokemon.id < 10000) {
+        pokeId.innerHTML = `#${pokemon.id}`
+    }
+
+
+
+
+    const pokeTypeFlex = document.createElement('ul')
+    pokeTypeFlex.classList.add('type-flex')
+
+    getTypes(pokemon, pokeTypeFlex)
+
+
+
+
+
+    pokeInfoBox.append(pokeSprite, pokeId, pokeName, pokeTypeFlex)
+    card.append(pokeInfoBox);
+
+}
+
+
+
+
+function getTypes(pokemon, typeList) {
+
+    pokemon.types.forEach(tayp => {
+        const pokeType = document.createElement('li')
+        pokeType.innerText = capitalizeFirstLetter(tayp.type.name)
+        const typeBg = {
+            grass: "#9bcc50",
+            poison: "#b97fc9",
+            fire: "#fd7d24",
+            flying: "linear-gradient(to bottom, #19d3f3 50%, #bdbdbd 50%)",
+            water: "#4592c4",
+            bug: "#729f3f",
+            normal: "#a4acaf",
+            ground: "linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)",
+            electric: "#f7de3f",
+            fairy: "#fdb9e9",
+            fighting: "#d56723",
+            psychic: "#f366b9",
+            rock: "#a38c21",
+            dark: "#707070",
+            ice: "#51c4e7",
+            ghost: "#7b62a3",
+            steel: "#9eb7b8",
+            dragon: "linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)"
+        }
+
+        const typeColors = {
+            grass: "#212121",
+            poison: "white",
+            fire: "white",
+            flying: "#212121",
+            water: "white",
+            bug: "white",
+            normal: "#212121",
+            ground: "#212121",
+            electric: "#212121",
+            fairy: "#212121",
+            fighting: "white",
+            psychic: "white",
+            rock: "white",
+            dark: "white",
+            ice: "#212121",
+            ghost: "white",
+            steel: "#212121",
+            dragon: "white"
+        }
+
+        if (typeColors[tayp.type.name]) {
+            pokeType.style.background = typeBg[tayp.type.name]
+            pokeType.style.color = typeColors[tayp.type.name]
+
+        }
+
+
+
+        typeList.append(pokeType)
+    });
+
+}
+
+
+
+
+// will caps first letter
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+//randomized number
+
+const randomizer = (max, min) => {
+
+    const randomizedNum = Math.floor(Math.random() * (max - min + 1) + min)
+    return randomizedNum
+}
+
+
+
